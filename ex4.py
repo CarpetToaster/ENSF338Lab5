a200b5a0900efb29cd1ad381df3be90e419831f2
JasonNguyen3 <jason.nguyen3@ucalgary.ca>
2025-03-05 18:33:29 -0700

    finished ex4

---
+++ ex4.py
@@ -0,0 +1,155 @@
+import random
+import timeit
+import matplotlib.pyplot as plt
+
+# Part 1
+
+class ArrayQueue:
+    def __init__(self, size):
+        self.size = size
+        self.queue = [None] * size
+        self.head = -1
+        self.tail = -1
+
+    def isEmpty(self):
+        return self.head == -1
+
+    def isFull(self):
+        return (self.tail + 1) % self.size == self.head
+
+    def enqueue(self, item):
+        if self.isFull():
+            print("Queue is full")
+            return
+        elif self.isEmpty():
+            self.head = 0
+            self.tail = 0
+        else:
+            self.tail = (self.tail + 1) % self.size
+        self.queue[self.tail] = item
+
+
+    def dequeue(self):
+        if self.isEmpty():
+            print("Queue is empty")
+            return None
+        item = self.queue[self.head]
+        if self.head == self.tail:
+            self.head = -1
+            self.tail = -1
+        else:
+            self.head = (self.head + 1) % self.size
+        return item
+
+# Part 2
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+    def getData(self):
+        return self.value
+
+    def setData(self, value):
+        self.value = value
+
+    def getNext(self):
+        return self.next
+
+    def setNext(self, next):
+        self.next = next
+
+
+class LinkedQueue:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def isEmpty(self):
+        return self.head == None
+
+    def enqueue(self, item):
+        node = Node(item)
+        if self.head == None:
+            self.head = node
+            self.tail = node
+        else:
+            self.tail.setNext(node)
+            self.tail = node
+
+
+
+    def dequeue(self):
+        if self.isEmpty():
+            print("Queue is empty")
+            return None
+        item = self.head.getData()
+        self.head = self.head.getNext()
+        if self.head == None:
+            self.tail = None
+        return item
+
+# Part 3
+def randomTasks(tasks):
+    taskList = []
+    for i in range (tasks):
+        probabilityValue = random.random()
+
+        if probabilityValue <= 0.7:
+            taskList.append("enqueue")
+
+        else:
+            taskList.append("dequeue")
+    return taskList
+
+# Part 4
+
+def performTaskList(taskList, queueType):
+    if queueType == "ArrayQueue":
+        queue = ArrayQueue(len(taskList))
+    else:
+        queue = LinkedQueue()
+
+    for task in taskList:
+        if task == "enqueue":
+            queue.enqueue(1)
+        else:
+            queue.dequeue()
+
+# Part 4
+
+tasks = 10000
+trials = 100
+taskList = randomTasks(tasks)
+
+arrayTimes = []
+linkedTimes = []
+
+for i in range(trials):
+    randomTasks(tasks)
+    arrayTime = timeit.timeit("performTaskList(taskList, 'ArrayQueue')", globals = globals(), number = 5)
+    linkedTime = timeit.timeit("performTaskList(taskList, 'LinkedQueue')", globals = globals(), number = 5)
+    arrayTimes.append(arrayTime)
+    linkedTimes.append(linkedTime)
+
+print(arrayTimes)
+print(linkedTimes)
+
+# Part 5
+x = range(len(arrayTimes))
+
+plt.plot(x, arrayTimes, label="ArrayQueue Times", color='blue')
+plt.plot(x, linkedTimes, label="LinkedQueue Times", color='red')
+plt.xlabel("Trial Number")
+plt.ylabel("Time (seconds)")
+plt.title("Performance Comparison of ArrayQueue and LinkedQueue")
+plt.legend()
+plt.show()
+
+# Part 5 discussion
+# The graph shows the both implementations have linear time complexity, and are take similar amounts of time
+# to execute. However, the linked lists implementation is slightly faster in this case.
+# This could be due to the array implementation performing more checks/comparisons than the linked lists implementation
+# as well as the array queue performing more calculations. Where the array version performs modulus operations,
+# the linked lists version simply works with pointers.
