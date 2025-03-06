0d0d1a055537b147a5d5e38d0b5641052e77df6f
CarpetToaster <davidjosephibrahim@gmail.com>
2025-03-05 20:30:52 -0700

    methinks ex1 is done...? i might come back and add improvements later but this was slightly exhausting

--- ex1.py
+++ ex1.py
@@ -1,4 +1,4 @@
-import re
+import sys
 
 class Node:
     def __init__(self, data, next = None):
@@ -8,10 +8,12 @@ class Node:
 class CalculatorStack:
     def __init__(self, head:Node = None):
         self.head = head
+        self.size = 0 if head == None else 1
         
     def push(self, data):
         newNode = Node(data, self.head)
         self.head = newNode
+        self.size += 1
         
     def pop(self):
         if self.head == None:
@@ -19,6 +21,7 @@ class CalculatorStack:
         
         returnee = self.head.data
         self.head = self.head.next
+        self.size -= 1
         
         return returnee
     
@@ -52,7 +55,6 @@ while pointer != None:
 print("****************")
 print(test.peek())
 '''
-ops = ['(', ')', '+', '-', '*', '/']
 
 def add(a, b):
     return a + b
@@ -74,20 +76,64 @@ doOp = {
     "/": div
 }
 
+prio = {
+    "+": 1,
+    "-": 1,
+    "*": 0,
+    "/": 0,
+    None: 9999
+}
+
+# this implementation is a (slightly modified and probably misunderstood) version of the shunting yard alogrithm.
 def main():
-    operatorStack = CalculatorStack()
-    numericStack = CalculatorStack()
-    expression = input("Input a valid expression: ")
+    shunt = CalculatorStack()
+    postfix = CalculatorStack() # I'm pretty sure this is supposed to be a queue normally but since that doesn't seem to be allowed, we'll have to reverse this lol
     
+    if len(sys.argv) != 2:
+        print("need to have an expression as a single string!")
+        exit(0)
+    expression = sys.argv[1]
+    
+    # arrange into correct order of operations
     for char in expression:
-        if char in ops:
-            operatorStack.push(char)
-            expression = expression.replace(char, '')
+        if char == '(':
+            shunt.push(char)
             
-    numberTokens = expression.split(" ")
-    for token in numberTokens:
-        numericStack.push(token)
-
-
+        if char in doOp:
+            if shunt.peek() in doOp and prio[char] > prio[shunt.peek()]:
+                postfix.push(shunt.pop())
+            shunt.push(char)    
+                
+        elif char == ')':
+            while shunt.peek() != '(':
+                postfix.push(shunt.pop())
+                
+            # and get rid of the closing bracket as well.
+            if shunt.peek() == '(':
+                shunt.pop()
+                
+        elif char.isnumeric():
+            postfix.push(char)
+            
+    # Now reverse the postfix
+    truePostfix = CalculatorStack()
+    while postfix.size != 0:
+        truePostfix.push(postfix.pop())
+    
+    # and finally, for some actual caclulations...
+    numStack = CalculatorStack()
+    while truePostfix.size != 0:
+        
+        if truePostfix.peek().isnumeric():
+            numStack.push(truePostfix.pop())
+        
+        elif truePostfix.peek() in doOp:
+            num2 = int(numStack.pop())
+            num1 = int(numStack.pop())
+            res = doOp[truePostfix.pop()](num1, num2)
+            numStack.push(res)
+            
+    print(numStack.pop())
+              
 main()
         
